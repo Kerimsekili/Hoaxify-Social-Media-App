c5fe3f339ff89751e018bf6f903dfa651e45a499
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-11-10 14:43:50 +0300

    Creating Database, genericResponse, UserRepository

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,18 +1,23 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserService userService;
 
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    
+
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user){
-        log.info(user.toString());
+    @ResponseStatus(HttpStatus.CREATED)
+    public GenericResponse createUser(@RequestBody User user){
+        userService.save(user);
+        return new GenericResponse("User created!");
+
     }
 }
