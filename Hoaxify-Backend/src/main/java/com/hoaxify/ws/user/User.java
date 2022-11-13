ae638d2fe61ea9240f933e3222dd729542fc21b6
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-11-13 22:14:54 +0300

    adding i18n Language support(eng-tr)

--- Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/User.java
+++ Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/User.java
@@ -21,7 +21,7 @@ public class User {
     @GeneratedValue()
     private long id;
 
-    @NotNull
+    @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
     @Size(min = 4,max = 255)
     @UniqueUsername
     private String username;
@@ -33,7 +33,7 @@ public class User {
 
     @NotNull
     @Size(min = 8 ,max = 255)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",message = "{hoaxify.constraint.username.Pattern.message}")
     private String password;
 
 }
