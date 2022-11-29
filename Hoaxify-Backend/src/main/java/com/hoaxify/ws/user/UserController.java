c4165b6a11e54170bc95778203b33c96f0dc2fd5
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-11-29 16:39:14 +0300

    Error handler for All Error cases

--- Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserController.java
+++ Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -27,16 +27,4 @@ public class UserController {
         userService.save(user);
         return new GenericResponse("User created!");
     }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception){
-        ApiError error = new ApiError(400,"Validation Error","/api/1.0/users");
-        Map<String ,String > validationErrors = new HashMap<>();
-        for (FieldError fieldError :exception.getBindingResult().getFieldErrors()){
-            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
-        }
-        error.setValidationErrors(validationErrors);
-        return error;
-    }
 }
