731cdfab3b17e68345117a43b9148d22c8876ac7
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-12-16 01:53:40 +0300

    Adding UserQuery and NotFoundException

--- Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserController.java
+++ Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,26 +5,33 @@ import com.hoaxify.ws.user.vm.UserVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("api/1.0")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/1.0/users")
+    @PostMapping("/users")
     public GenericResponse createUser(@Valid @RequestBody User user) {
         userService.save(user);
         return new GenericResponse("User created!");
     }
 
-    @GetMapping("/api/1.0/users")
+    @GetMapping("/users")
     Page<UserVM> getUsers(Pageable page) {
         return userService.getUsers(page).map(UserVM::new);
     }
+
+    @GetMapping("/users/{username}")
+    UserVM getUser(@PathVariable String username){
+        User user = userService.getByUsername(username);
+        return new UserVM(user);
+    }
+
 }
+
+
--- Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserService.java
+++ Hoaxify-Backend/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,5 +1,6 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,18 +17,26 @@ public class UserService {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository,PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         super();
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public void save(User user){
+    public void save(User user) {
         user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     public Page<User> getUsers(Pageable page) {
         return userRepository.findAll(page);
+    }
+
+    public User getByUsername(String username) {
+        User inDB =  userRepository.findByUsername(username);
+        if (inDB == null){
+            throw new NotFoundException();
+        }
+        return inDB;
     }
 }
